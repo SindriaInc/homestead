b84ec2566b16f9e72b4d472c16f52afd1666cd3e
lucapitzoi <lucapitzoi.sindria.org>
2018-12-13 14:44:32 +0100

    Clean garbage files

--- scripts/serve-magento22.sh
+++ scripts/serve-magento22.sh
@@ -20,15 +20,18 @@ else configureZray=""
 fi
 
 
-cat "/var/www/$1/vendor/sindria/homestead/resources/magento22/nginx/sites-available/sindria-homestead-magento2.2.conf" > "/etc/nginx/sites-available/$1"
+cat "/var/www/$1/vendor/sindria/homestead/resources/magento22/magento2.2.conf" > "/etc/nginx/sites-available/$1"
 cat "/var/www/$1/vendor/sindria/homestead/resources/magento22/php.ini.sample" > "/etc/php/7.1/fpm/php.ini"
 ln -fs "/etc/nginx/sites-available/$1" "/etc/nginx/sites-enabled/$1"
 mkdir /var/log/nginx/$1/
 touch /var/log/nginx/$1/access.log
 touch /var/log/nginx/$1/error.log
+
+apt install php7.1-mcrypt php7.1-zip php7.1-intl -y
+
 systemctl restart php7.1-fpm
 systemctl restart nginx
-#sudo apt-get install php7.1-mcrypt -y
+
 
 
 
